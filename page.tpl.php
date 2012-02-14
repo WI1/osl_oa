b82600f63455ab9a1da7bc0138b5283927c3ca31
Dominik Böhler <dominik.boehler@wiso.uni-erlangen.de>
2012-02-14 12:26:50 +0100

    changed wrong css after removing the unnecessary styles

--- page.tpl.php
+++ page.tpl.php
@@ -7,7 +7,6 @@
   <?php print $content_region ?>
 </div></div>
 <div id='right'><div class='page-region'>
-  <?php if ($page_tools): ?><?php print $page_tools ?><?php endif; ?>
   <?php if ($right) print $right ?>
 </div></div>
 
