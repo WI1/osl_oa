5d79f29c1ab27b8f39a7c1256a867908163e50a7
Harisuman Naik <Harisuman.Naik@wi1.wiso.uni-erlangen.de>
2014-06-10 07:38:53 -0500

    increased username ..  limit

--- template.php
+++ template.php
@@ -503,3 +503,42 @@ function _osl_oa_icon_class($path) {
   $path = drupal_get_path_alias($path);
   return str_replace('/', '-', $path);
 }
+
+/**
+ * Username override
+ * Hides or shows username '(not verified)' text
+ */
+function osl_oa_username($object) {
+  if ($object->uid && $object->name) {
+    // Shorten the name when it is too long or it will break many tables.
+    if (drupal_strlen($object->name) > 35) {
+      $name = drupal_substr($object->name, 0, 35) .'...';
+    }
+    else {
+      $name = $object->name;
+    }
+    if (user_access('access user profiles')) {
+      $output = l($name, 'user/'. $object->uid, array('title' => t('View user profile.')));
+    }
+    else {
+      $output = check_plain($name);
+    }
+  }
+  else if ($object->name) {
+    // Sometimes modules display content composed by people who are
+    // not registered members of the site (e.g. mailing list or news
+    // aggregator modules). This clause enables modules to display
+    // the true author of the content.
+    if ($object->homepage) {
+      $output = l($object->name, $object->homepage);
+    }
+    else {
+      $output = check_plain($object->name);
+    }
+    $output .= ' ('. t('not verified') .')';
+  }
+  else {
+    $output = variable_get('anonymous', t('Anonymous'));
+  }
+  return $output;
+}
